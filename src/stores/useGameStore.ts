ddc6f2227cee30eac2242a6cdb713397844c63cb
mikanorm39 <mikanorm@outlook.jp>
2025-12-18 15:56:23 +0900

    refactor: reorganize project structure, move models to public, add src directory with game components

---
+++ src/stores/useGameStore.ts
@@ -0,0 +1,13 @@
+import create from 'zustand';
+
+type State = {
+  score: number;
+  addScore: (n: number) => void;
+};
+
+export const useGameStore = create<State>((set) => ({
+  score: 0,
+  addScore: (n) => set((s) => ({ score: s.score + n })),
+}));
+
+export default useGameStore;
